7547c498ea74d0f68f3b1aaa47a261569355f94b
oirving <oirving@thoughtworks.com>
2014-09-16 23:26:32 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DreTech2::Application.config.secret_key_base = 'ff7cf4486a8ff5a38f289ea568d43bf6dbbf46df34364f9130c69219b5dfe2676968c765d8b4b0b5e28557efc79764ce3f02eb2f88f80dd3516f94d98ccc2eca'
